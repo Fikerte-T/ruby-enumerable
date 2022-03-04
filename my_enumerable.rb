2692d576760c01ef06a4a2267aae963bc45efd5d
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-03-04 15:38:28 +0100

    implement any? and filter methods

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -6,5 +6,21 @@ module MyEnumerable
         }
       value
     end
-  
+
+    def any?
+      value = false
+        each{@list.
+          each{|n| return true if yield n}
+        }
+      value
+    end
+
+    def filter
+      value = []
+        each{@list.
+          each{|n| value.push(n) if yield n}
+        }
+      value
+    end
+
 end
--- my_list.rb
+++ my_list.rb
@@ -8,3 +8,10 @@ class MyList
     end
 
 end
+list = MyList.new(1, 2, 3, 4)
+
+p list.all? {|e| e < 5}
+p list.all? {|e| e > 5}
+p list.any? {|e| e == 2}
+p list.any? {|e| e == 5}
+p list.filter {|e| e.even?}
