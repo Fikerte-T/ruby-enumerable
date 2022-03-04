4c48cc3924aff1608daed3ebf0b66193ed72b004
fikerte <tofikertesfaye@gmail.com>
2022-03-04 18:05:04 +0300

    Fix linter errors

--- my_list.rb
+++ my_list.rb
@@ -1,5 +1,5 @@
-include MyEnumerable
 class MyList
+  include MyEnumerable
   def initialize(*list)
     @list = list
   end
@@ -10,8 +10,8 @@ class MyList
 end
 list = MyList.new(1, 2, 3, 4)
 
-p list.all? { |e| e < 5 }
-p list.all? { |e| e > 5 }
-p list.any? { |e| e == 2 }
-p list.any? { |e| e == 5 }
-p list.filter { |e| e.even? }
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
+list.filter(&:even?)
